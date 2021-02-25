f7b1b609350ead50e47e1d7171f6fee811159b74
I Nyoman Putra Yoga <inyomanpyoga25@gmail.com>
2021-02-25 16:44:17 +0800

    Tugas-1

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <tittle></tittle>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+    <div class="jumbotron text-center">
+
+        <header>
+            <img src="./media/bibit.png">
+            <h2 style="margin:  0px;">Sistem Informasi Pertanian</h2>
+            <h4 style="margin:  0px;">Selamat Datang</h4>
+            <hr>
+
+
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Pupuk</a>
+                    </li>
+                    <li>
+                        <a href="#">Bibit</a>
+                    </li>
+                    <li>
+                        <a href="#">Peptisida</a>
+                    </li>
+                    <li>
+                        <a href="#">tutorial</a>
+                    </li>
+                </ul>
+            </nav>
+            <header>
+    </div>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <hr>
+                    <h2 class="text-center">Home</h2>
+                    <hr>
+                </div>
+            </div>
+
+
+            <div class="row text-center">
+                <div class="col-lg-6 colom1">
+                    <h3 class=text-center>Bibit</h3>
+                    <img src="./media/images.jpg" alt="">
+                    <p>
+                        bibit adalah bahan tanam yang berasal dari benih yang disemai dan telah berkecambah untuk
+                        menuju proses pertumbuhan dan perkembangan tanam selanjutnya. Ciri dari bibit adalah sudah
+                        tumbuhnya akar, daun dan batang.
+                    </p>
+                </div>
+                <div class="col-lg-6 colom2">
+                    <h3 class=text-center>Pupuk</h3>
+                    <img src="./media/pupuk1.png">
+                    <p>
+                        Pupuk adalah material yang ditambahkan pada media tanam atau tanaman
+                        untuk mencukupi kebutuhan hara yang diperlukan tanaman sehingga mampu
+                        berproduksi dengan baik. Material pupuk dapat berupa bahan organik ataupun
+                        non-organik (mineral). Pupuk berbeda dari suplemen. Pupuk mengandung bahan baku yang
+                        diperlukan pertumbuhan dan perkembangan tanaman, sementara suplemen seperti hormon
+                        tumbuhan membantu kelancaran proses metabolisme. Meskipun demikian, ke dalam pupuk,
+                        khususnya pupuk buatan, dapat ditambahkan sejumlah material suplemen.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>copyrigth 2021 | built by Putra Yoga</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
